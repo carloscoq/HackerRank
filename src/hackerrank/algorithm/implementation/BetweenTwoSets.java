a265bc171b3e4497502462f63d6a36eeda5299a9
Carlos Coqueiro <ces.coqueiro@gmail.com>
2017-11-03 15:10:47 -0300

    BetweenTwoSets solved

--- src/hackerrank/algorithm/implementation/BetweenTwoSets.java
+++ src/hackerrank/algorithm/implementation/BetweenTwoSets.java
@@ -4,7 +4,62 @@ import java.util.Scanner;
 
 public class BetweenTwoSets {
 	static int getTotalX(int[] a, int[] b) {
-		return 0;
+		int total = 0;
+		int x = findHighestFromA(a);
+		int menorB = findLowestFromB(b);
+		
+		while(x<=menorB){
+/*			System.out.println("valor de x: "+x);
+*/			int countA = 0;
+			int countB = 0;
+			for(int i = 0; i< a.length; i++){
+				if(x%a[i] == 0){
+/*					System.out.println("encontrado: "+x+ "valor a: "+ a[i]);
+*/					countA++;
+				}
+			}
+			System.out.println("contador: "+ countA);
+			if(countA == a.length){
+				
+				for (int i : b) {
+					if(i%x == 0){
+/*						System.out.println("encontrado: "+x+" valor b: "+i );
+*/						countB++;
+					}
+				}
+				if(countB == b.length){
+					
+					total++;
+				}
+			}
+			x++;
+		}
+		
+		return total;
+	}
+	
+	static int findHighestFromA(int[] a){
+		int highestA = 0;
+		
+		for (int i = 0; i<a.length; i++){
+			if(a[i] > highestA){
+				highestA = a[i];
+			}
+		}
+		
+		return highestA;
+	}
+	
+	static int findLowestFromB(int[] b){
+		int lowest = 100;
+		
+		for (int i = 0; i<b.length; i++){
+			if(b[i] < lowest){
+				lowest = b[i];
+			}
+		}
+		
+		return lowest;
 	}
 
 	public static void main(String[] args) {
